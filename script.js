4e5cff45de70eea9fbc10122e6e218b7289a86d6
Swetha <103497687+Swethakanuka@users.noreply.github.com>
2023-05-30 18:40:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["Director","Indian Actor","Director","Film Editor","Politician","Government Official","Producer"],
+    typeSpeed:120,
+    backSpeed:80,
+    loop:true
+});
